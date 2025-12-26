66d1c2e161ef32643fcde89accd947fbf114180e
troh <dhxofla123@naver.com>
2025-12-26 23:31:00 +0900

    fragment 이동

---
+++ ch03/Plants/app/src/main/java/com/example/plants/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.plants
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+const val PLANET_ID = "PLANET_ID"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
